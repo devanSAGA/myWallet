2b6b12c84cb228828ed4e89694dc6aec283d4479
Devansh <officialdevansh@gmail.com>
2019-09-10 19:48:56 +0530

    Add firebase config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvy3O5YDTPauqSV0yUONu10ojlk7jRzAQ",
+  authDomain: "mywallet-867dc.firebaseapp.com",
+  databaseURL: "https://mywallet-867dc.firebaseio.com",
+  projectId: "mywallet-867dc",
+  storageBucket: "mywallet-867dc.appspot.com",
+  messagingSenderId: "1062510890914",
+  appId: "1:1062510890914:web:f289609509349fadd42cfb"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);

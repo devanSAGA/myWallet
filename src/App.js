f4a140009846b3844574109ca25115510171954d
Devansh <officialdevansh@gmail.com>
2019-09-11 12:41:28 +0530

    remove dummy data

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
 import React, { Component } from "react";
 import AppRouter from "./routers/AppRouter";
 import { Provider } from "react-redux";
-import moment from "moment";
 import configureStore from "./store";
-import { addExpense } from "./actions/expenses";
-import getFilteredExpenses from "./selectors/expensesSelector";
 
 const store = configureStore();
 
-store.dispatch(
-  addExpense({
-    note: "Water Bill",
-    amount: 4000,
-    createdAt: 0
-  })
-);
-store.dispatch(
-  addExpense({
-    note: "Rent Bill",
-    amount: 6000,
-    createdAt: 0
-  })
-);
-store.dispatch(
-  addExpense({
-    note: "Car EMI",
-    amount: 15000,
-    createdAt: 0
-  })
-);
-const state = store.getState();
-const visibleExpenses = getFilteredExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// store.dispatch(
+//   addExpense({
+//     note: "Water Bill",
+//     amount: 4000,
+//     createdAt: 0
+//   })
+// );
+// store.dispatch(
+//   addExpense({
+//     note: "Rent Bill",
+//     amount: 6000,
+//     createdAt: 0
+//   })
+// );
+// store.dispatch(
+//   addExpense({
+//     note: "Car EMI",
+//     amount: 15000,
+//     createdAt: 0
+//   })
+// );
 class App extends Component {
   render() {
     return (

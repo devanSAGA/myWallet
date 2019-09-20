64ff1e646b3a22c39f47060753eef9fbb71c834d
Devansh <officialdevansh@gmail.com>
2019-09-20 11:41:24 +0530

    put sensitive data in environment variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvy3O5YDTPauqSV0yUONu10ojlk7jRzAQ",
-  authDomain: "mywallet-867dc.firebaseapp.com",
-  databaseURL: "https://mywallet-867dc.firebaseio.com",
-  projectId: "mywallet-867dc",
-  storageBucket: "mywallet-867dc.appspot.com",
-  messagingSenderId: "1062510890914",
-  appId: "1:1062510890914:web:f289609509349fadd42cfb"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
